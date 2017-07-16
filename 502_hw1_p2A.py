b11552a12e56d73d80941ac868aac75016df0c4e
carriexu24 <cxu24@usfca.edu>
2017-07-15 21:35:24 -0700

    add notes to 502_hw1_p2

--- 502_hw1_p2A.py
+++ 502_hw1_p2A.py
@@ -1,38 +1,50 @@
 import numpy
 
+#define Eliminate
 def Eliminate(A, b):
     m = len(A[0])
     n = len(A)
 
-    if m > n:
+    #A should be a square matrix
+    if m != n:
+        print "You should input a n*n A."
         return StandardError
 
+    #for each column of A
     for i in range(m):
+
+        #set the location of the pivot
         j = i
+
+        #if the pivot == 0, then move to the next row
         while A[j][i] == 0:
             j += 1
+
+            #if in every row the pivot == 0, return an error
             if j == n:
                 print "This equation can't be solved."
                 return StandardError
 
+        #if the pivot has been moved, change the rows
         if j > i:
             myline = A[i]
             A[i] = A[j]
             A[j] = myline
 
+        #calculate the scaler, and do eliminations
         for t in range(i+1, n):
             scaler = A[t][i] / A[i][i]
             for r in range(m):
                 A[t][r] = A[t][r] - scaler * A[i][r]
-            #print t
-            #print A[t]
             b[t] = b[t] - scaler * b[i]
 
+        #print the results of each elimination
         print A
         print b
 
     return (A, b)
 
+#define A
 A = []
 for i in range(10):
     myline = []
@@ -40,17 +52,18 @@ for i in range(10):
         myline.append(10*i+j+1)
     A.append(myline)
 
-#define array v
+#define v
 v = []
 for i in range(10):
     v.append(i+1)
 
-#define array b
+#define b
 b = 10*[1]
 
-#build a 10 * 10 array R(e)
+#build a 10 * 10 R(e)
 R = numpy.random.rand(10, 10)
 
+#make copies
 A1 = [row[:] for row in A]
 A2 = [row[:] for row in A]
 v1 = v[:]
--- 502_hw1_p2B.py
+++ 502_hw1_p2B.py
@@ -1,53 +1,71 @@
 import time
 import numpy
 
+#define Eliminate
 def Eliminate(A, b):
     m = len(A[0])
     n = len(A)
 
-    if m > n:
+    #A should be a square matrix
+    if m != n:
+        print "You should input a n*n A."
         return StandardError
 
+    #for each column of A
     for i in range(m):
+
+        #set the location of the pivot
         j = i
+
+        #if the pivot == 0, then move to the next row
         while A[j][i] == 0:
             j += 1
+
+            #if in every row the pivot == 0, return an error
             if j == n:
                 print "This equation can't be solved."
                 return StandardError
 
+        #if the pivot has been moved, change the rows
         if j > i:
             myline = A[i]
             A[i] = A[j]
             A[j] = myline
 
+        #calculate the scaler, and do eliminations
         for t in range(i+1, n):
             scaler = A[t][i] / A[i][i]
             for r in range(m):
                 A[t][r] = A[t][r] - scaler * A[i][r]
-            #print t
-            #print A[t]
             b[t] = b[t] - scaler * b[i]
 
     return (A, b)
 
+#list of matrix size n
 n = [100, 500, 1000, 5000, 10000]
+
+#list of time using linalg.solve
 timeL = []
+
+#list of time using Eliminate.py
 timeE = []
 
+#for each size
 for i in n:
 
-    #build a n * n array R(e)
+    #build a n * n R(e)
     R = numpy.random.rand(i, i)
 
     #build w
     w = i * [1]
 
+    #record the time using linalg.solve
     time1 = time.clock()
     x = numpy.linalg.solve(R, w)
     time2 = time.clock()
     timeL.append(time2 - time1)
 
+    #record the time using Eliminate.py
     time3 = time.clock()
     Eliminate(R, w)
     time4 = time.clock()
